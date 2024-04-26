71a5be36e09fd61c10e36bb860cddf9ad6d49bf0
goyalpalash <122611794+goyalpalash@users.noreply.github.com>
2024-04-26 16:26:26 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,3 +1,5 @@
+
+    
 class Solution(object):
     def isValid(self, s):
         """
@@ -5,7 +7,18 @@ class Solution(object):
         :rtype: bool
         """
         pass
-    
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
 
 
 
